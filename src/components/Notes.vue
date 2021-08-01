76534d04bc6b429c29547b3413fb80a2c01cd430
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-08-02 00:56:46 +0300

    feat: add toggle classes

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,8 +1,13 @@
 <template>
   <!-- note list -->
   <ul class="notes">
-    <li class="note" v-for="(note, index) in notes" :key="index">
-      <div class="note-header">
+    <li
+      class="note"
+      :class="{ full: !grid }"
+      v-for="(note, index) in notes"
+      :key="index"
+    >
+      <div class="note-header" :class="{ full: !grid }">
         <p>{{ note.title }}</p>
         <p @click="removeNote(index)">x</p>
       </div>
@@ -19,6 +24,10 @@ export default {
   props: {
     notes: {
       type: Array,
+      required: true,
+    },
+    grid: {
+      type: Boolean,
       required: true,
     },
   },
@@ -44,15 +53,22 @@ export default {
   width: calc((100% - 40px) / 2);
   padding: 20px;
   background-color: #fff;
+  box-shadow: 0 30px 30px rgba($color: #000000, $alpha: 0.02);
+  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
 
-  @media screen and (max-width: 768px) {
-    width: 100%;
+  &:hover {
+    box-shadow: 0 30px 30px rgba($color: #000000, $alpha: 0.04);
+    transform: translate(0, -6px);
+    transition-delay: 0s !important;
   }
-}
 
-.note + .note {
-  @media screen and (max-width: 768px) {
-    margin-top: 40px;
+  &.full {
+    width: 100%;
+    text-align: center;
+
+    &:first-child {
+      margin-bottom: 40px;
+    }
   }
 }
 
@@ -65,6 +81,10 @@ export default {
   justify-content: space-between;
   align-items: center;
 
+  h1 {
+    font-size: 32px;
+  }
+
   p {
     color: #494ce8;
     font-size: 22px;
@@ -72,6 +92,31 @@ export default {
 
     &:last-of-type {
       cursor: pointer;
+    }
+  }
+
+  svg {
+    color: #999;
+    cursor: pointer;
+
+    &.active {
+      color: #494ce8;
+    }
+  }
+
+  svg + svg {
+    margin-left: 12px;
+  }
+
+  &.full {
+    justify-content: center;
+    align-items: flex-start;
+
+    p {
+      &:first-child {
+        flex-grow: 1;
+        margin: 0 32px;
+      }
     }
   }
 }
