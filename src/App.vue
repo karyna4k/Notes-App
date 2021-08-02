3196628bacc72a43e3664003044061351d6bf790
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-08-02 18:29:54 +0300

    feat: add Search component

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,58 @@
           <!-- title -->
           <div class="note-header">
             <h1>{{ title }}</h1>
+
+            <Search
+              :value="search"
+              placeholder="Find your note"
+              @search="search = $event"
+            />
+
+            <!-- icons control -->
             <div class="icons">
-              <svg :class="{active: grid}" @click="grid = true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
-              <svg :class="{active: !grid}" @click="grid = false" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
+              <svg
+                :class="{ active: grid }"
+                @click="grid = true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <rect x="3" y="3" width="7" height="7"></rect>
+                <rect x="14" y="3" width="7" height="7"></rect>
+                <rect x="14" y="14" width="7" height="7"></rect>
+                <rect x="3" y="14" width="7" height="7"></rect>
+              </svg>
+              <svg
+                :class="{ active: !grid }"
+                @click="grid = false"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <line x1="8" y1="6" x2="21" y2="6"></line>
+                <line x1="8" y1="12" x2="21" y2="12"></line>
+                <line x1="8" y1="18" x2="21" y2="18"></line>
+                <line x1="3" y1="6" x2="3" y2="6"></line>
+                <line x1="3" y1="12" x2="3" y2="12"></line>
+                <line x1="3" y1="18" x2="3" y2="18"></line>
+              </svg>
             </div>
           </div>
 
           <!-- note list -->
-          <Notes :notes="notes" :grid="grid" @remove="removeNote" />
+          <Notes :notes="notesFilter" :grid="grid" @remove="removeNote" />
         </div>
       </section>
     </div>
@@ -30,16 +74,19 @@
 import Message from "@/components/Message.vue";
 import NewNote from "./components/NewNote.vue";
 import Notes from "./components/Notes.vue";
+import Search from "./components/Search.vue";
 
 export default {
   components: {
     Message,
     NewNote,
     Notes,
+    Search,
   },
   data() {
     return {
       title: "Notes App",
+      search: "",
       message: null,
       grid: true,
       note: {
@@ -64,6 +111,24 @@ export default {
         },
       ],
     };
+  },
+  computed: {
+    notesFilter() {
+      let array = this.notes;
+      let search = this.search;
+
+      if (!search) return array;
+      // small
+      search = search.trim().toLowerCase();
+      // filter
+      array = array.filter((item) => {
+        if (item.title.toLowerCase().indexOf(search) !== -1) {
+          return item;
+        }
+      });
+      // error
+      return array;
+    },
   },
   methods: {
     addNote() {
